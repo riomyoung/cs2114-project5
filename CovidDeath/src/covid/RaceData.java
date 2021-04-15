1097dc65a4df2f671cd18b816fad430f0993a3c5
Rio Young <riomyoung@gmail.com>
2021-04-15 18:42:23 -0400

    -Created methods for RaceData

--- CovidDeath/src/covid/RaceData.java
+++ CovidDeath/src/covid/RaceData.java
@@ -2,4 +2,41 @@ package covid;
 
 public class RaceData {
 
+    private String name;
+    private int cases;
+    private int deaths;
+    private double cfRatio;
+    
+    public RaceData( String name, int cases, int deaths )
+    {
+        this.name = name;
+        this.cases = cases;
+        this.deaths = deaths;
+        cfRatio = ( deaths / cases ) * 100;
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public int getCases()
+    {
+        return cases;
+    }
+    
+    public int getDeaths()
+    {
+        return deaths;
+    }
+    
+    public double getCFRatio()
+    {
+        return cfRatio;
+    }
+    
+    public String toString()
+    {
+        return name + ": " + cases + " cases, " + cfRatio + "% CFR";
+    }
 }
